635a7bc8d9634544ab48889aedc4ec46fa783142
Rysias <jvhithamar@gmail.com>
2021-09-03 16:59:29 +0200

    create very raw data pipeline

--- BERTopic_inital_exploration.py
+++ BERTopic_inital_exploration.py
@@ -6,12 +6,19 @@ Main goals of exploration:
     - Looking at computational cost (is it feasible to run on a CPU?)
     - Exploring flexibility (how much info can I get out?)
     - Familiarizing myself with the API 
+    
+Initial Results: 
+    - Default parameters works pretty badly with small-ish data
+    - Relatively easy to use
 
 Created on Fri Sep  3 16:11:04 2021
 
 @author: jhr
 """
 import random
+import numpy as np
+import pandas as pd
+from umap import UMAP
 from bertopic import BERTopic
 from sklearn.datasets import fetch_20newsgroups
 
@@ -20,5 +27,13 @@ docs = fetch_20newsgroups(subset='test',  remove=('headers', 'footers', 'quotes'
 docs = random.sample(docs, 1000)
 
 # initializing the model #
-topic_model = BERTopic(embedding_model="paraphrase-MiniLM-L6-v2")
+# Model choice: fastest model I could find on sentence-transformers
+topic_model = BERTopic(embedding_model="paraphrase-MiniLM-L6-v2") 
 topics, probs = topic_model.fit_transform(docs)
+
+print(topic_model.get_topic_info())
+
+
+# Getting the centroids #
+embeddings = topic_model._extract_embeddings(docs)
+
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # TransTopicXAI
 Exploration of topic modelling for transformers 📚🤖
+
+## Overview of data pipeline
+How should the data look to facilitate semantic search? Below is a list of the data "flow"
+1. Documents (docs): raw texts -->
+2. Clean doc: (optionally) cleaned documents (stop words, lower cased etc). This step is not necessary for the pipeline but might improve performance. -->
+3. Embedded docs: Documents are embedded using some kind of sentence-transformer solution. Output is saved
+4. Topic creation: Following the BERTopic-pipeline (LINK), a list of topics are created. How these exactly are represented is currently unclear, so I will have to read up on HDBScan.
+5. ???
+6. Final representation: Each document should have a similarity score to each of the topics (after a fitting amount has been selected). The similarity scores should ideally be normalized. If there are n documents and k topics, this will represent a (n, k) matrix with normalized scores.
