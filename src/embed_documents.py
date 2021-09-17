e02a45c9b93a88f6ce0b9e68fac63a13f46fca6f
Rysias <jvhithamar@gmail.com>
2021-09-17 14:11:39 +0200

    Create output dictionary

--- src/embed_documents.py
+++ src/embed_documents.py
@@ -2,19 +2,28 @@
 Pipeline for embedding documents. 
 This is for making the documents ready for BERTopic 
 """
+import numpy as np
 import pickle
 from pathlib import Path
 from sentence_transformers import SentenceTransformer
 
+
 def read_pickle(file_path: Path):
     with open(file_path, "rb") as f:
         return pickle.load(f)
+
 
 DATA_DIR = Path("../BscThesisData/data")
 data_path = DATA_DIR / "paragraph_dict.pkl"
 resume_dict = read_pickle(data_path)
 
 # Powering up the transformer!
-sentence_model = SentenceTransformer("Maltehb/-l-ctra-danish-electra-small-cased")
+sentence_model = SentenceTransformer(
+    "Maltehb/-l-ctra-danish-electra-small-cased")
 
-embedding_shape = sentence_model.encode("hejsa").shape
+embedding_dimension = sentence_model.encode("hejsa").shape[0]
+
+# Creating the output dictionary
+embedding_dict = {doc_id: np.zeros((len(paragraphs), embedding_dimension))
+                  for doc_id, paragraphs in resume_dict.items()}
+
