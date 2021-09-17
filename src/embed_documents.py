4ffbf674304893c6dfbd86a98316269c0f564175
Rysias <jvhithamar@gmail.com>
2021-09-17 14:45:49 +0200

    Comment code

--- src/embed_documents.py
+++ src/embed_documents.py
@@ -9,6 +9,11 @@ from sentence_transformers import SentenceTransformer
 from typing import List
 
 
+def serialize_data(dat, file_path):
+    with open(file_path, "wb") as f:
+        pickle.dump(dat, f)
+
+
 def flatten_list(lst: List):
     return [elem for sublist in lst for elem in sublist]
 
@@ -28,16 +33,20 @@ sentence_model = SentenceTransformer(
 
 embedding_dimension = sentence_model.encode("hejsa").shape[0]
 
-# Creating the output dictionary
+# Embedding the documents #
+all_paragraphs = flatten_list(list(resume_dict.values()))
+all_embeds = sentence_model.encode(all_paragraphs, show_progress_bar=True)
+
+# Add embeddings to dictionary #
+# Creating the output dictionary structure
 embedding_dict = {doc_id: np.zeros((len(paragraphs), embedding_dimension))
                   for doc_id, paragraphs in resume_dict.items()}
 
-all_paragraphs = flatten_list(list(resume_dict.values()))
-
-all_embeds = sentence_model.encode(all_paragraphs, show_progress_bar=True)
-
 current_idx = 0
 for doc_id, emb_array in embedding_dict.items():
-    stop_idx = current_idx + emb_array.shape[0]
+    stop_idx = current_idx + emb_array.shape[0]  # Makes it stop at the right number of docs
     embedding_dict[doc_id] += all_embeds[current_idx:stop_idx, :]
-    current_idx += emb_array.shape[0]
+    current_idx += emb_array.shape[0]  # Reset to next document
+
+# Writing data to disk
+serialize_data(embedding_dict, DATA_DIR / "embedding_dict")
