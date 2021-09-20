121b9ca2cfb04ed885300c1c8633c18c1008ec8a
Rysias <jvhithamar@gmail.com>
2021-09-20 13:02:17 +0200

    Add stopwords to pipeline

--- src/embed_documents.py
+++ src/embed_documents.py
@@ -4,6 +4,7 @@ This is for making the documents ready for BERTopic
 """
 import numpy as np
 import pickle
+import requests
 from pathlib import Path
 from sentence_transformers import SentenceTransformer
 from typing import List
@@ -23,6 +24,12 @@ def read_pickle(file_path: Path):
         return pickle.load(f)
 
 
+def load_text_url(text_url: str): 
+    """Inputs a URL of a newline seperated text-file and returns a list"""
+    response = requests.get(text_url)
+    return response.text.split("\n")
+
+    
 DATA_DIR = Path("../BscThesisData/data")
 data_path = DATA_DIR / "paragraph_dict.pkl"
 resume_dict = read_pickle(data_path)
@@ -33,8 +40,14 @@ sentence_model = SentenceTransformer(
 
 embedding_dimension = sentence_model.encode("hejsa").shape[0]
 
-# Embedding the documents #
+
+# Preprocess embeddings
 all_paragraphs = flatten_list(list(resume_dict.values()))
+stop_word_url = "https://gist.githubusercontent.com/berteltorp/0cf8a0c7afea7f25ed754f24cfc2467b/raw/305d8e3930cc419e909d49d4b489c9773f75b2d6/stopord.txt"
+stop_words = load_text_url(stop_word_url)
+
+
+# Embedding the documents #
 all_embeds = sentence_model.encode(all_paragraphs, show_progress_bar=True)
 
 # Add embeddings to dictionary #
