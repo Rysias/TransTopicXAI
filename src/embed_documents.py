74b8fe474dd2bc03b514b71f4dfbb731415f8419
Rysias <jvhithamar@gmail.com>
2021-09-20 13:33:42 +0200

    add stopwords removal to pipeline

--- src/embed_documents.py
+++ src/embed_documents.py
@@ -5,6 +5,7 @@ This is for making the documents ready for BERTopic
 import numpy as np
 import pickle
 import requests
+from spacy import load
 from pathlib import Path
 from sentence_transformers import SentenceTransformer
 from typing import List
@@ -30,6 +31,11 @@ def load_text_url(text_url: str):
     return response.text.split("\n")
 
     
+def remove_stopwords(doc, stop_words):
+    """ Removes the stopwords from a spacy doc given a list of stopwords"""
+    return " ".join([word.text for word in doc if not word.text.lower() in stop_words])
+
+
 DATA_DIR = Path("../BscThesisData/data")
 data_path = DATA_DIR / "paragraph_dict.pkl"
 resume_dict = read_pickle(data_path)
@@ -46,7 +52,10 @@ all_paragraphs = flatten_list(list(resume_dict.values()))
 stop_word_url = "https://gist.githubusercontent.com/berteltorp/0cf8a0c7afea7f25ed754f24cfc2467b/raw/305d8e3930cc419e909d49d4b489c9773f75b2d6/stopord.txt"
 stop_words = load_text_url(stop_word_url)
 
+danish_nlp = load("da_core_news_sm")
 
+tokenized_paragraphs = danish_nlp.tokenizer.pipe(all_paragraphs)
+clean_paragraphs = [remove_stopwords(doc, stop_words) for doc in tokenized_paragraphs]
 # Embedding the documents #
 all_embeds = sentence_model.encode(all_paragraphs, show_progress_bar=True)
 
