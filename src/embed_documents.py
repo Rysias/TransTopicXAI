f01af208456b743a72969ce62d46bfa11edb8ad3
Rysias <jvhithamar@gmail.com>
2021-09-22 11:57:06 +0200

    revert embeddings

--- src/embed_documents.py
+++ src/embed_documents.py
@@ -4,8 +4,6 @@ This is for making the documents ready for BERTopic
 """
 import numpy as np
 import pickle
-import requests
-from spacy import load
 from pathlib import Path
 from sentence_transformers import SentenceTransformer
 from typing import List
@@ -25,16 +23,6 @@ def read_pickle(file_path: Path):
         return pickle.load(f)
 
 
-def load_text_url(text_url: str): 
-    """Inputs a URL of a newline seperated text-file and returns a list"""
-    response = requests.get(text_url)
-    return response.text.split("\n")
-
-    
-def remove_stopwords(doc, stop_words):
-    """ Removes the stopwords from a spacy doc given a list of stopwords"""
-    return " ".join([word.text for word in doc if not word.text.lower() in stop_words])
-
 DATA_DIR = Path("../BscThesisData/data")
 data_path = DATA_DIR / "paragraph_dict.pkl"
 resume_dict = read_pickle(data_path)
@@ -44,22 +32,9 @@ sentence_model = SentenceTransformer(
     "Maltehb/-l-ctra-danish-electra-small-cased")
 
 embedding_dimension = sentence_model.encode("hejsa").shape[0]
-
-
-# Preprocess embeddings
-print("cleaning paragraphs!")
 all_paragraphs = flatten_list(list(resume_dict.values()))
-stop_word_url = "https://gist.githubusercontent.com/berteltorp/0cf8a0c7afea7f25ed754f24cfc2467b/raw/305d8e3930cc419e909d49d4b489c9773f75b2d6/stopord.txt"
-stop_words = load_text_url(stop_word_url)
 
-danish_nlp = load("da_core_news_sm")
 
-tokenized_paragraphs = danish_nlp.tokenizer.pipe(all_paragraphs)
-clean_paragraphs = [remove_stopwords(doc, stop_words) for doc in tokenized_paragraphs]
-
-serialize_data(clean_paragraphs, DATA_DIR / "clean_paragraphs.pkl")
-
-print("done cleaning paragraphs!")
 # Embedding the documents #
 print("Embedding documents!")
 all_embeds = sentence_model.encode(all_paragraphs, show_progress_bar=True)
