00089e2f44289c790f9d2f4f7304376a1e2f006a
Rysias <jvhithamar@gmail.com>
2021-10-05 14:05:09 +0200

    explore functionality

--- src/explainlp_exploration.py
+++ src/explainlp_exploration.py
@@ -1,6 +1,19 @@
 import pandas as pd
+import pickle
+import numpy as np
 from explainlp.explainlp import ClearSearch
 from pathlib import Path
+
+
+def read_pickle(file_path):
+    with open(file_path, "rb") as f:
+        return pickle.load(f)
+
+
+def flatten_embeddings(embedding_dict):
+    """Creates a big matrix with all the embeddings from the dict"""
+    return np.vstack(embedding_dict.values())
+
 
 model = ClearSearch(
     "Maltehb/-l-ctra-danish-electra-small-cased",
@@ -10,3 +23,10 @@ model = ClearSearch(
 
 DATA_DIR = Path("../BscThesisData/data")
 doc_topics = pd.read_csv(DATA_DIR / "doc_topics.csv")
+embeddings_dict = read_pickle(DATA_DIR / "embedding_dict.pkl")
+embeddings = flatten_embeddings(embeddings_dict)
+
+
+model.calculate_centroids(doc_topics["topic"].values, doc_topics["prob"], embeddings)
+
+model.transform_many(embeddings_dict.values())
