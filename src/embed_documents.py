ef497291ecafbf9c60a1577814597ed9752b6b14
Rysias <jvhithamar@gmail.com>
2021-10-13 13:25:57 +0200

    Add new transformers

--- src/embed_documents.py
+++ src/embed_documents.py
@@ -9,6 +9,7 @@ from pathlib import Path
 from sentence_transformers import SentenceTransformer
 from typing import List
 
+
 def serialize_data(dat, file_path):
     with open(file_path, "wb") as f:
         pickle.dump(dat, f)
@@ -22,18 +23,24 @@ def read_pickle(file_path: Path):
     with open(file_path, "rb") as f:
         return pickle.load(f)
 
+
 def create_embedding_dict(resume_dict, embeddings, embedding_dimension):
     # Add embeddings to dictionary #
     # Creating the output dictionary structure
-    embedding_dict = {doc_id: np.zeros((len(paragraphs), embedding_dimension))
-                    for doc_id, paragraphs in resume_dict.items()}
+    embedding_dict = {
+        doc_id: np.zeros((len(paragraphs), embedding_dimension))
+        for doc_id, paragraphs in resume_dict.items()
+    }
 
     current_idx = 0
     for doc_id, emb_array in embedding_dict.items():
-        stop_idx = current_idx + emb_array.shape[0]  # Makes it stop at the right number of docs
+        stop_idx = (
+            current_idx + emb_array.shape[0]
+        )  # Makes it stop at the right number of docs
         embedding_dict[doc_id] += embeddings[current_idx:stop_idx, :]
         current_idx += emb_array.shape[0]  # Reset to next document
     return embedding_dict
+
 
 def main(args):
     DATA_DIR = Path("../BscThesisData/data")
@@ -41,7 +48,12 @@ def main(args):
     resume_dict = read_pickle(data_path)
     all_paragraphs = flatten_list(list(resume_dict.values()))
     # Powering up the transformer!
-    transformer_list = ["Maltehb/-l-ctra-danish-electra-small-cased", "saattrupdan/nbailab-base-ner-scandi"]
+    transformer_list = [
+        "chcaa/da_dacy_small_trf",
+        "chcaa/da_dacy_medium_trf",
+        "chcaa/da_dacy_large_trf",
+        "saattrupdan/nbailab-base-ner-scandi",
+    ]
 
     for transformer in transformer_list:
         print(f"ready for {transformer}!")
@@ -49,18 +61,21 @@ def main(args):
         transformer_name = transformer.split("/")[1]
         # Boot it up
         sentence_model = SentenceTransformer(transformer)
-    
+
         # Embedding the documents #
         print("Embedding documents!")
         all_embeds = sentence_model.encode(all_paragraphs, show_progress_bar=True)
         embedding_dimension = sentence_model.encode("en").shape[0]
-        embedding_dict = create_embedding_dict(resume_dict, all_embeds, embedding_dimension)
+        embedding_dict = create_embedding_dict(
+            resume_dict, all_embeds, embedding_dimension
+        )
 
         print("done with embedding...")
         # Writing data to disk
         embedding_path = DATA_DIR / f"{transformer_name}_embedding_dict.pkl"
         serialize_data(embedding_dict, embedding_path)
     print("all done!")
+
 
 if __name__ == "__main__":
     my_parser = argparse.ArgumentParser(description="Embed Documents")
