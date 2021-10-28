16dbbae6d80f9592a6e674d006abaef3f8277fe5
Rysias <jvhithamar@gmail.com>
2021-10-28 13:45:34 +0200

    fix k-fold

--- src/training_code.py
+++ src/training_code.py
@@ -2,26 +2,35 @@ import pandas as pd
 import numpy as np
 from simpletransformers.classification import ClassificationModel, ClassificationArgs
 from pathlib import Path
-from sklearn.model_selection import GroupKFold, train_test_split, GroupShuffleSplit
+from sklearn.model_selection import GroupKFold, train_test_split
+from typing import Generator, Tuple
+
+
+def create_k_folds(
+    train_df: pd.DataFrame, k=3
+) -> Generator[None, Tuple[pd.DataFrame, pd.DataFrame], None]:
+    group_kfold = GroupKFold(n_splits=k)
+    for train_idx, test_idx in group_kfold.split(
+        train_df.drop("case_id", axis=1),
+        train_df["label"],
+        train_df["case_id"],
+    ):
+        yield train_idx, test_idx
 
 
 all_data = pd.read_csv(Path("../../BscThesisData/data/full_df.csv"))
 unique_ids = all_data["case_id"].unique()
 train_ids, test_ids = train_test_split(unique_ids, random_state=42, train_size=0.8)
 
-training_data = all_data.loc[np.isin(all_data["case_id"], train_ids)]
+training_data = all_data.loc[np.isin(all_data["case_id"], train_ids)].reset_index(
+    drop=True
+)
 labels = training_data["label"].unique()
 label_dict = {label: i for i, label in enumerate(labels)}
 training_data = training_data.replace({"label": label_dict})
 
-training_data.drop("case_id", axis=1)
-group_kfold = GroupKFold(n_splits=3)
-for train_idx, test_idx in group_kfold.split(
-    training_data.drop("case_id", axis=1),
-    training_data["label"],
-    training_data["case_id"],
-):
-    print(train_idx)
+for train_id, val_id in create_k_folds(training_data):
+    print(training_data.loc[train_id, :])
     break
 
 
