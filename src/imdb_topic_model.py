bcf055b402e8ec89800be4a49ade6b908c92f5f9
rysias <jvhithamar@gmail.com>
2021-11-26 10:28:42 +0100

    create nice lil pipeline

--- src/imdb_topic_model.py
+++ src/imdb_topic_model.py
@@ -1,4 +1,4 @@
-from typing import Dict, List
+from typing import Dict, List, Union
 import pandas as pd
 import numpy as np
 from pathlib import Path
@@ -12,6 +12,10 @@ def get_model_name(file_path: Path) -> str:
     return full_name[:stop_idx]
 
 
+def get_train_filter(train_index: pd.Index) -> np.ndarray:
+    return np.isin(np.arange(50000), train_index)
+
+
 # Paths
 DATA_DIR = Path("../data")
 MODEL_PATH = Path("../models")
@@ -20,43 +24,36 @@ if __name__ == "__main__":
     embedding_paths = list(DATA_DIR.glob("*embs.npy"))
 
     # Load data
-    train_data = pd.read_csv(DATA_DIR / "imdb_train.csv")
-    ids = np.load(DATA_DIR / "all_ids.npy", allow_pickle=True)
+    train_data = pd.read_csv(DATA_DIR / "imdb_train.csv", index_col=0)
+    train_filter = get_train_filter(train_data.index)
 
-    train_data.set_index(train_data["id"], inplace=True)
-    id_df = pd.DataFrame(ids, columns=["id"]).set_index("id")
-
-    train_data.index.unique()
-    id_df.join(train_data, how="inner")
     # Create vectorizer
     # Looping over the models
     for emb_path in embedding_paths:
         model_name = get_model_name(emb_path)
         print(f"processing {model_name}")
 
-        embeddings = np.load(DATA_DIR / emb_path)
-        train_mask = np.isin(ids, train_data["id"].values)
+        all_embs = np.load(DATA_DIR / emb_path)
+        embeddings = all_embs[train_filter, :]
 
         # Fitting models
         print("fitting model...")
-        topic_model = BERTopic(vectorizer_model=vectorizer_model, nr_topics=10)
-        topics, probs = topic_model.fit_transform(full_docs, full_embs)
+        vectorizer_model = CountVectorizer(ngram_range=(1, 2), stop_words="english")
+        topic_model = BERTopic(
+            low_memory=True,
+            vectorizer_model=vectorizer_model,
+            verbose=True,
+            nr_topics=10,
+        )
+        topics, probs = topic_model.fit_transform(train_data["text"], embeddings)
 
         # Saving predictions
         print("saving predictions...")
         preds_df = pd.DataFrame(
-            list(zip(topics, probs, full_docs)), columns=["topic", "prob", "doc"]
+            list(zip(topics, probs, train_data["text"])),
+            columns=["topic", "prob", "doc"],
         )
         preds_df.to_csv(DATA_DIR / f"{model_name}_doc_topics.csv", index=False)
 
         print("saving model")
         topic_model.save(str(MODEL_PATH / f"{model_name}_topic_model"))
-
-        # Saving topics
-        topic_dict = topic_model.get_topics()
-        topic_dict_clean = {
-            k: [tup[0] for tup in word_list]
-            for k, word_list in topic_dict.items()
-            if k != -1
-        }
-        pickle_object(topic_dict_clean, DATA_DIR / f"{model_name}_topic_dict.pkl")
