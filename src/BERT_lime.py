127d262ae596fe9fe6e1283d5a946210b14c5be8
rysias <jvhithamar@gmail.com>
2021-11-29 14:20:53 +0100

    create loop for bert explanations

---
+++ src/BERT_lime.py
@@ -0,0 +1,73 @@
+from typing import Dict, List, Union
+import pandas as pd
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+from pysentimiento import create_analyzer
+from lime.lime_text import LimeTextExplainer
+from pysentimiento.analyzer import AnalyzerOutput
+
+DATA_DIR = Path("data/")
+
+
+def sort_sentiment(res: AnalyzerOutput) -> np.array:
+    vals = [res.probas[k] for k in LABELS]
+    return np.array(vals).reshape(1, -1)
+
+
+def list_to_arr(result: List[AnalyzerOutput]) -> np.ndarray:
+    return np.vstack([sort_sentiment(out) for out in result])
+
+
+def format_output(result: Union[List[AnalyzerOutput], AnalyzerOutput]) -> np.ndarray:
+    try:
+        return sort_sentiment(result)
+    except AttributeError:
+        return list_to_arr(result)
+
+
+def dict_to_arr(dct: dict) -> np.ndarray:
+    n_feats = len(dct.values())
+    return np.array(list(dct.values())).reshape((-1, n_feats))
+
+
+def predict_proba(sentence: str) -> np.ndarray:
+    pred = SENTIMENT.predict(sentence)
+    return format_output(pred)
+
+
+def get_pred_idx(sentence: str) -> int:
+    return np.argmax(predict_proba(sentence))
+
+
+def pred_loop(df: pd.DataFrame, explainer: LimeTextExplainer):
+    for i, row in df.iterrows():
+        pred_idx = get_pred_idx(row["cleantext"])
+        explanation = explainer.explain_instance(
+            row["cleantext"],
+            predict_proba,
+            num_features=5,
+            labels=pred_idx,
+        )
+        fig = explanation.as_pyplot_figure()
+        plt.savefig(DATA_DIR / f"bertexp_{i}.png")
+
+
+def main():
+    explainer = LimeTextExplainer(class_names=LABELS)
+    # Test on data
+    df = pd.read_csv(DATA_DIR / "bert_test.csv", index_col=0)
+    # Pred and Plot stuff
+    pred_loop(df, explainer)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Create BERT explanation PNGs")
+    parser.add_argument("--data-dir", type=str, help="Where to find and load the data")
+
+    args = parser.parse_args()
+    DATA_DIR = Path(args.data_dir)
+    SENTIMENT = create_analyzer("sentiment", lang="en")
+    LABELS = list(SENTIMENT.id2label.values())
+    main()
