7bda784947b4cff2e921e1b73707ed1006ba6d94
rysias <jvhithamar@gmail.com>
2021-12-03 13:13:04 +0100

    honky tonk

--- src/explainlp_train.py
+++ src/explainlp_train.py
@@ -1,78 +1,33 @@
 from typing import Dict, List
 import numpy as np
 import pandas as pd
-import pickle
 from pathlib import Path
-import sys
-import train_topic_models as tta
-from bertopic import BERTopic
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import f1_score
-from sklearn.model_selection import GridSearchCV
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.pipeline import make_pipeline
-
-sys.path.append("C:\\Users\\jhr\\ExplainlpTwitter\\")
-from explainlp.clearformer import Clearformer
-
-DATA_DIR = Path("../../BscThesisData/data")
-MODEL_PATH = Path("../models")
+from sklearn.metrics.pairwise import cosine_similarity
 
 
-def read_pickle(file_path: Path):
-    with open(file_path, "rb") as f:
-        return pickle.load(f)
+MODEL_PATH = Path("../ExplainlpTwitter/output/topic_model")
+EMB_PATH = Path("../ExplainlpTwitter/output/embeddings.npy")
+
+doc_topics = pd.read_csv(Path("data") / "full_doc_topics.csv", index_col=0)
+cool_idx = doc_topics["topic"] != -1
+non_null_topics = doc_topics[cool_idx]["topic"].values
+
+topic_ids = np.unique(non_null_topics)
 
 
-def create_embedding_df(embedding_dict: Dict[str, List[np.ndarray]]) -> pd.DataFrame:
-    doc_embeddings = {k: emb.mean(axis=0) for k, emb in embedding_dict.items()}
-    return pd.DataFrame.from_dict(doc_embeddings, orient="index")
+full_embs = np.load(EMB_PATH)
+small_embs = full_embs[cool_idx, 1:]
 
 
-label_df = pd.read_csv(DATA_DIR / "emne_labels.csv")
-embedding_paths = list(DATA_DIR.glob("*_embedding_dict.pkl"))
-embedding_dict = read_pickle(embedding_paths[0])
-embedding_df = create_embedding_df(embedding_dict)
-model_name = tta.get_model_name(embedding_paths[0])
-topic_df = pd.read_csv(DATA_DIR / f"{model_name}_doc_topics.csv")
+centroids = np.zeros((len(topic_ids), small_embs.shape[1]))
+for i in topic_ids:
+    centroids[i, :] += np.mean(small_embs[non_null_topics == i, :], axis=0)
 
-# Fitting clearformer
-topic_model = BERTopic.load(str(MODEL_PATH / f"{model_name}_topic_model"))
+np.save(Path("data/new_centroids.npy"), centroids)
 
-clearformer = Clearformer(topic_model)
-embs = embedding_df.dropna().to_numpy()
-full_x = np.hstack(
-    (
-        topic_df["topic"].values.reshape(-1, 1),
-        topic_df["prob"].values.reshape(-1, 1),
-        embs,
-    )
+
+topic_embs = np.vstack(
+    (cosine_similarity(arr, centroids) for arr in np.split(full_embs[:, 1:], 5))
 )
-clearformer.fit(full_x)
+np.save(Path("data/topic_embs.npy"), topic_embs)
 
-
-label_df.set_index("case_id", inplace=True)
-fit_df = label_df.join(embedding_df).dropna()
-y = fit_df["label"]
-X = fit_df.drop("label", axis=1).to_numpy()
-X_new = clearformer.transform(X)
-
-# Training model
-clf = LogisticRegression(class_weight="balanced")
-clf.fit(X_new, y)
-
-grid_values = {"C": np.logspace(-2, 2, 20)}
-grid_clf_acc = GridSearchCV(clf, param_grid=grid_values, scoring="accuracy")
-
-
-pipeline = make_pipeline(MinMaxScaler(), grid_clf_acc)
-pipeline.fit(X_new, y)
-
-f1_score(y, pipeline.predict(X_new), average=None)
-f1_score(y, pipeline.predict(X_new), average="weighted")
-
-pipeline[-1].best_params_
-
-from pprint import pprint
-
-pprint(topic_model.get_topics())
