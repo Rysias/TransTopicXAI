776d1e8b999be5c7f16b2e4def4d28a4add8c7eb
rysias <jvhithamar@gmail.com>
2021-12-06 14:42:23 +0100

    change to umap embeddings

--- src/explainlp_train.py
+++ src/explainlp_train.py
@@ -1,41 +1,32 @@
 from typing import Dict, List
 import numpy as np
 import pandas as pd
+import hdbscan
 from pathlib import Path
 from bertopic import BERTopic
 from sklearn.metrics.pairwise import cosine_similarity
 
 
-MODEL_PATH = Path("../ExplainlpTwitter/output/topic_model")
-EMB_PATH = Path("../ExplainlpTwitter/output/embeddings.npy")
+MODEL_PATH = next(Path("models").glob("*_topic_model"))
+EMB_PATH = Path("data/umap_embs.npy")
 
-topic_model = BERTopic.load(MODEL_PATH)
 
-doc_topics = pd.read_csv(Path("data") / "full_doc_topics.csv", index_col=0)
+doc_path = next(Path("data").glob("*full_doc_topics_*.csv"))
+doc_topics = pd.read_csv(doc_path, index_col=0)
 cool_idx = doc_topics["topic"] != -1
 non_null_topics = doc_topics[cool_idx]["topic"].values
 
-topic_ids = np.unique(non_null_topics)
+topic_ids, cnts = np.unique(non_null_topics, return_counts=True)
 
 
 full_embs = np.load(EMB_PATH)
-smaller_embs = np.vstack(
-    (topic_model.umap_model.transform(emb) for emb in np.split(full_embs[:, 1:], 16))
-)
-
-
-small_embs = full_embs[cool_idx, 1:]
-
+small_embs = full_embs[cool_idx, :]
 
 centroids = np.zeros((len(topic_ids), small_embs.shape[1]))
 for i in topic_ids:
     centroids[i, :] += np.mean(small_embs[non_null_topics == i, :], axis=0)
 
-np.save(Path("data/new_centroids.npy"), centroids)
-
-
-topic_embs = np.vstack(
-    (cosine_similarity(arr, centroids) for arr in np.split(full_embs[:, 1:], 5))
-)
+np.save(Path("data/newer_centroids.npy"), centroids)
+topic_embs = cosine_similarity(full_embs, centroids)
 np.save(Path("data/topic_embs.npy"), topic_embs)
 
