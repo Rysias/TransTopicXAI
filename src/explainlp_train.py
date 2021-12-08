c96830bdf383fb0a8f3d3987a6ff529b96d48e5c
rysias <jvhithamar@gmail.com>
2021-12-08 08:36:13 +0100

    finish up clearformer pipeline

--- src/explainlp_train.py
+++ src/explainlp_train.py
@@ -1,4 +1,5 @@
-from typing import Dict, List
+from datetime import datetime
+import joblib
 import numpy as np
 import argparse
 import pandas as pd
@@ -11,35 +12,36 @@ def latest_pattern(dr: Path, pat: str) -> Path:
     return list(dr.glob(pat))[-1]
 
 
+def load_topic_model(dr: Path) -> BERTopic:
+    topic_path = latest_pattern(dr, "*_topic_model")
+    return BERTopic.load(topic_path)
+
+
+def load_embeds(dr: Path) -> np.ndarray:
+    embedding_path = latest_pattern(dr, "*bertweet-base-sentiment-analysis_embs.npy")
+    return np.load(embedding_path)
+
+
+def load_topics(dr: Path) -> pd.Series:
+    doc_topics = latest_pattern(dr, "*full_doc_topics_*.csv")
+    return pd.read_csv(doc_topics, usecols=["topic"])["topic"]
+
+
 def main(args):
+    nowtime = datetime.now().strftime("%Y%m%d_%H%M%S")
     DATA_DIR = Path(args.data_dir)
-    doc_topics = latest_pattern(DATA_DIR, "*full_doc_topics_*.csv")
-    embeddings = latest_pattern(DATA_DIR, "*bertweet-base-sentiment-analysis_embs.npy")
-    topic_model = latest_pattern(DATA_DIR, "*_topic_model")
+    doc_topics = load_topics(DATA_DIR)
+    embeddings = load_embeds(DATA_DIR)
+    topic_model = load_topic_model(DATA_DIR)
     clearformer = Clearformer(topic_model)
 
-    filter_idx = doc_topics["topic"] != -1
-    non_null_topics = doc_topics.loc[filter_idx, "topic"].values
-
-
-doc_path = next(Path("data").glob("*full_doc_topics_*.csv"))
-doc_topics = pd.read_csv(doc_path, index_col=0)
-cool_idx = doc_topics["topic"] != -1
-non_null_topics = doc_topics[cool_idx]["topic"].values
-
-topic_ids, cnts = np.unique(non_null_topics, return_counts=True)
-
-
-full_embs = np.load(EMB_PATH)
-small_embs = full_embs[cool_idx, :]
-
-centroids = np.zeros((len(topic_ids), small_embs.shape[1]))
-for i in topic_ids:
-    centroids[i, :] += np.mean(small_embs[non_null_topics == i, :], axis=0)
-
-np.save(Path("data/newer_centroids.npy"), centroids)
-topic_embs = cosine_similarity(full_embs, centroids)
-np.save(Path("data/topic_embs.npy"), topic_embs)
+    filter_idx = doc_topics != -1
+    non_null_topics = doc_topics.loc[filter_idx].values
+    filtered_embeddings = embeddings[filter_idx, :]
+    X = np.hstack((non_null_topics.reshape(-1, 1), filtered_embeddings))
+    topic_embs = clearformer.fit_transform(X)
+    np.save(DATA_DIR / "topic_embs_{nowtime}.npy", topic_embs)
+    joblib.dump(clearformer, DATA_DIR / f"clearformer_{nowtime}.joblib")
 
 
 if __name__ == "__main__":
