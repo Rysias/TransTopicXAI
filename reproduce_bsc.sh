530f2dc1c6432b571a7f9e8317818036e3220249
rysias <jvhithamar@gmail.com>
2021-12-14 11:56:41 +0100

    start on reproducibility script

---
+++ reproduce_bsc.sh
@@ -0,0 +1,28 @@
+eval "$(conda shell.bash hook)"
+echo 'Setting up embedding environment...'
+conda env create -f embenv.yml
+echo 'setting up twitter data'
+cd ..
+git clone https://github.com/cardiffnlp/tweeteval.git
+cd TransTopciXAI
+mkdir data models data/explains
+cd src
+conda activate embenv
+python tweeteval_data.py
+echo 'embedding data'
+python embed_documents_df.py --data-path "data/tweeteval_text.csv" --embedding-path "data/"
+conda deactivate
+echo 'done embedding data!'
+echo 'Setting up topic-model environment'
+conda env create -f topicmodel.yml
+conda activate rapids-21.12
+cd ../..
+git clone https://github.com/Rysias/clearformers.git
+cd clearformers 
+pip install .
+echo "done!"
+echo 'Training topic-model'
+cd ../TransTopciXAI/src
+python create_topic.py --data-path "data/tweeteval_text.csv" --embedding-path "data/bertweet-base-sentiment-analysis_embs.npy" --data-size 45000 --save-path "models"
+
+
