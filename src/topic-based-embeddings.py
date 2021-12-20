846b07d4ef8b17b0b57efa0a5fcd71459c120d43
rysias <jvhithamar@gmail.com>
2021-12-20 11:35:55 +0100

    modify clearformer pipeline and add logging

--- src/topic-based-embeddings.py
+++ src/topic-based-embeddings.py
@@ -2,6 +2,7 @@ from datetime import datetime
 import numpy as np
 import pandas as pd
 import argparse
+import logging
 import joblib
 from pathlib import Path
 from sklearn.model_selection import GridSearchCV
@@ -28,27 +29,41 @@ def load_target(dr: Path, settype="train"):
 
 
 def main(args):
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s %(levelname)-8s %(message)s",
+        datefmt="%d-%m-%Y:%H:%M:%S",
+    )
+
     current_time = datetime.now().strftime("%y%m%d%H")
     DATA_DIR = Path(args.data_dir)
-
+    logging.info("Loading data...")
     all_embs = load_embeddings(DATA_DIR)
     Y_train = load_target(DATA_DIR, settype="train")
     Y_test = load_target(DATA_DIR, settype="test")
-    X_train = all_embs[Y_train.index.values, :]
-    X_test = all_embs[Y_test.index.values, :]
+    X_train_raw = all_embs[Y_train.index.values, :]
+    X_test_raw = all_embs[Y_test.index.values, :]
+    logging.info("Finished loading data!")
 
     # initializing topic model
+    logging.info("initializing topic modelling....")
     topic_model = load_latest_topic_model(Path(args.topic_dir))
     clearformer = Clearformer(topic_model)
 
+    # Creating topic embeddings
+    X_train = clearformer.fit_transform(X_train_raw)
+    X_test = clearformer.fit_transform(X_test_raw)
+    logging.info("Done with topic embeddings!")
+
+    logging.info("Commencing training...")
     normalizer = MinMaxScaler()
-    poly = PolynomialFeatures(interaction_only=True, include_bias=True)
+    # poly = PolynomialFeatures(interaction_only=True, include_bias=True)
     model = LogisticRegression(solver="saga", penalty="l1")
     grid = {"logistic__C": np.logspace(-1, 5, 10)}  # l1 lasso
     pipeline = Pipeline(
         steps=[
-            ("topic-embed", clearformer),
-            ("poly", poly),
+            # ("poly", poly),
             ("normalize", normalizer),
             ("logistic", model),
         ],
@@ -56,6 +71,8 @@ def main(args):
     )
     gridsearch = GridSearchCV(pipeline, param_grid=grid, cv=3, verbose=True, n_jobs=-1)
     gridsearch.fit(X_train, Y_train)
+    logging.info("done!")
+    logging.info("saving results...")
     y_preds = gridsearch.predict(X_test)
     pd.DataFrame({"y_true": Y_test, "y_pred": y_preds}).to_csv(
         DATA_DIR / f"topic_preds_{current_time}.csv", index=False
@@ -64,6 +81,7 @@ def main(args):
     joblib.dump(
         gridsearch.best_estimator_, DATA_DIR / f"topic_predictor_{current_time}.joblib"
     )
+    logging.info("All done!")
 
 
 if __name__ == "__main__":
