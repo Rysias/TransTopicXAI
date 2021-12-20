af7874bd143cc357856077bc429ac45dfd9e6eb2
rysias <jvhithamar@gmail.com>
2021-12-20 15:33:54 +0100

    update with file write

--- src/get_topic_info.py
+++ src/get_topic_info.py
@@ -2,6 +2,8 @@ import pandas as pd
 import pickle
 from pprint import pprint
 from pathlib import Path
+from contextlib import redirect_stdout
+
 from typing import Dict, Tuple, List
 
 DATA_DIR = Path("../data")
@@ -22,17 +24,21 @@ def read_pickle(file_path):
         return pickle.load(f)
 
 
-doc_topics = pd.read_csv(latest_full_topics(DATA_DIR), index_col=0)
-clean_tweets = pd.read_csv(DATA_DIR / "tweeteval_text.csv", usecols=["text"])
-doc_topics = doc_topics.merge(clean_tweets, left_index=True, right_index=True)
-topic_words = read_pickle(DATA_DIR / "tweeteval_topic_dict.pkl")
-topic_dict = create_topic_dict(topic_words)
-for top, words in topic_dict.items():
-    print(f"evaluating topic {top}")
-    topic_tweets = doc_topics.loc[doc_topics["topic"] == top, "text"]
-    print(f"num tweets in {top}: {topic_tweets.shape[0]}")
-    pprint(f"{words = }")
-    print(f"examples for topic {top}")
-    example_tweets = topic_tweets.sample(10, random_state=42).tolist()
-    print(example_tweets)
-    print("")
+if __name__ == "__main__":
+    doc_topics = pd.read_csv(latest_full_topics(DATA_DIR), index_col=0)
+    clean_tweets = pd.read_csv(DATA_DIR / "tweeteval_text.csv", usecols=["text"])
+    doc_topics = doc_topics.merge(clean_tweets, left_index=True, right_index=True)
+    topic_words = read_pickle(DATA_DIR / "tweeteval_topic_dict.pkl")
+    topic_dict = create_topic_dict(topic_words)
+
+    with open(DATA_DIR / "topic_info.txt", "w", encoding="utf8") as f:
+        with redirect_stdout(f):
+            for top, words in topic_dict.items():
+                print(f"evaluating topic {top}")
+                topic_tweets = doc_topics.loc[doc_topics["topic"] == top, "text"]
+                print(f"num tweets: {topic_tweets.shape[0]}")
+                pprint(f"{words = }")
+                print(f"examples for topic {top}")
+                example_tweets = topic_tweets.sample(10, random_state=42).tolist()
+                print(example_tweets)
+                print("")
