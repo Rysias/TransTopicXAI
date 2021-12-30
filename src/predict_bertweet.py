94a2fac1514782c840681b09931ae41091364533
rysias <jvhithamar@gmail.com>
2021-12-30 11:24:15 +0100

    add break to bertweet preds

--- src/predict_bertweet.py
+++ src/predict_bertweet.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import sys
 from argparse import ArgumentParser
 from pysentimiento import create_analyzer
 from pathlib import Path
@@ -15,9 +16,13 @@ if __name__ == "__main__":
     parser.add_argument("--data-dir", default="../../final_proj_dat")
     args = parser.parse_args()
     DATA_DIR = Path(args.data_dir)
+    pred_path = DATA_DIR / "bertweet_preds.csv"
+    if pred_path.exists():
+        print("already exists")
+        sys.exit()
     test_df = pd.read_csv(DATA_DIR / "tweeteval_test.csv")
     sentiment = create_analyzer("sentiment", "en")
     preds = sentiment.predict(test_df["text"])
     parsed_preds = [parse_prediction(pred) for pred in preds]
     bertweet_preds = pd.DataFrame({"y_true": test_df["label"], "y_pred": parsed_preds})
-    bertweet_preds.to_csv(DATA_DIR / "bertweet_preds.csv")
+    bertweet_preds.to_csv(pred_path)
